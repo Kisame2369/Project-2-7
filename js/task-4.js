5e2ed8dea4711f076813af7b801794862e412538
BigBEE <165787884+IvanNesterenko2369@users.noreply.github.com>
2025-02-23 22:46:12 +0100

    fix

--- js/task-4.js
+++ js/task-4.js
@@ -4,14 +4,16 @@ form.addEventListener("submit", check);
 
 function check(event) {
   event.preventDefault();
-  const email = event.target.elements.email.value; 
+  const email = event.target.elements.email.value;
   const password = event.target.elements.password.value;
-  
+
   if (email === "" || password === "") {
-    return console.log("All form fields must be filled in");
+    alert("All form fields must be filled in");
+    return;
   }
 
-  console.log(`Email: ${email}, Password: ${password}`);
+  console.log({ email, password });
   event.target.reset();
 }
+
 
