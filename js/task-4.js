d5c15b3fb8ad1473f41661f82f9debc88fb0c92e
BigBEE <165787884+IvanNesterenko2369@users.noreply.github.com>
2025-02-23 12:18:16 +0100

    done

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,17 @@
+const form = document.querySelector(".login-form");
+
+form.addEventListener("submit", check);
+
+function check(event) {
+  event.preventDefault();
+  const email = event.target.elements.email.value; 
+  const password = event.target.elements.password.value;
+  
+  if (email === "" || password === "") {
+    return console.log("All form fields must be filled in");
+  }
+
+  console.log(`Email: ${email}, Password: ${password}`);
+  event.target.reset();
+}
 
--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+ 
+
+const button = document.querySelector(".change-color");
+const span = document.querySelector(".color");
+button.addEventListener("click", colorChange);
+function colorChange() {
+  const color = getRandomHexColor();
+  span.textContent = color;
+  document.body.style.backgroundColor = color;
+}
